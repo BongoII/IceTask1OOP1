907028c37191d6c7c944810b36a4988646b18f2d
Katlego II <162458360+BongoII@users.noreply.github.com>
2025-03-13 12:22:24 +0200

    Add files via upload

---
+++ IceTask1/Ingredients.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask1
+{
+    public class Ingredients
+    {
+        public int NumOfIngridients { get; set; }
+        public string[] Ingriedients { get; set; }
+        public int[] NumIngriedients { get; set; }
+        public string[] UnitOfMeasurement { get; set; }
+        public void enterIngridients()
+        {
+            for (int e = 0; e < NumOfIngridients; e++)
+            {
+                Console.Write("Enter the name of the ingridient: ");
+                Ingriedients[e] = Console.ReadLine();
+                Console.Write("\nEnter the quantity of the ingridients: ");
+                NumIngriedients[e] =Convert.ToInt32(Console.ReadLine());
+                Console.Write($"\nEnter the unit of measurement for {Ingriedients[e]}: ");
+                UnitOfMeasurement[e] = Console.ReadLine();
+
+            }
+        }
+        
+    }
+    public class Recipe : Ingredients
+    {
+        public string[] StepsForRecipe { get; set; }
+        public int NumSteps { get; set; }
+        public void GetFullSteps()
+        {
+            for(int v = 0; v < NumSteps; v++)
+            {
+                Console.WriteLine($"Step {v+1}");
+                Console.WriteLine(StepsForRecipe[v]);
+            }
+        }
+    }
+}
---
+++ IceTask1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe recipe = new Recipe();
+            Console.WriteLine("Enter number of ingridients");
+            recipe.NumOfIngridients= Convert.ToInt32(Console.ReadLine());
+            recipe.enterIngridients();
+            Console.WriteLine("Enter number of Steps to cook");
+            recipe.NumSteps = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In the following process you are going to describe each step and that step is going to be stored");
+            recipe.GetFullSteps();
+            
+        }
+    }
+}
